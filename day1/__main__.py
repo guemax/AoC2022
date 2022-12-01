390146bac20cd8c791d48a1dc0f082c9e0e93d26
guemax <max@example.com>
2022-12-01 20:30:53 +0100

    Remove data types from function and variable names

--- day1/__main__.py
+++ day1/__main__.py
@@ -3,15 +3,15 @@
 import numpy as np
 
 
-def get_list_of_total_calories_of_top_three_elves_with_the_most_calories(inventory_list: str) -> list:
-    list_of_total_calories_of_all_elves = []
+def get_total_calories_of_top_three_elves_carrying_most_calories(inventory_list: str) -> list:
+    total_calories_of_all_elves = []
 
     for individual_calories_of_this_elf in inventory_list.split("\n\n"):
         total_calories_of_this_elf = np.fromstring(individual_calories_of_this_elf, dtype=np.int_, sep="\n").sum()
-        list_of_total_calories_of_all_elves.append(total_calories_of_this_elf)
+        total_calories_of_all_elves.append(total_calories_of_this_elf)
 
-    list_of_total_calories_of_all_elves.sort()
-    return list_of_total_calories_of_all_elves[-3:]
+    total_calories_of_all_elves.sort()
+    return total_calories_of_all_elves[-3:]
 
 
 def get_input(file: str) -> str:
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     filenames = {"TEST INPUT": "day1/test_input.txt", "REAL INPUT": "day1/input.txt"}
     for type_of_input, filename in filenames.items():
         content = get_input(filename)
-        calories_of_top_three_elves = get_list_of_total_calories_of_top_three_elves_with_the_most_calories(content)
+        calories_of_top_three_elves = get_total_calories_of_top_three_elves_carrying_most_calories(content)
 
         print(f"\n{type_of_input}\n")
         print(f"--> Part 1: The N°1 Elf is carrying {max(calories_of_top_three_elves)} Calories in total.")
