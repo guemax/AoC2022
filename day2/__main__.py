f54307bb65535e812b0e7bdbd22ea1417cbf2023
guemax <max@example.com>
2022-12-02 13:40:34 +0100

    Set up output for solution
    Ready to start implementing!

--- day2/__main__.py
+++ day2/__main__.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
+
+def get_total_score(strategy_guide: str) -> np.int_:
+    pass
+
+
+def get_input(file: str) -> str:
+    with open(file, "r") as f:
+        return f.read()
+
 
 if __name__ == '__main__':
     print("\n======================")
 
     filenames = {"TEST INPUT": "day1/test_input.txt", "REAL INPUT": "day1/input.txt"}
     for type_of_input, filename in filenames.items():
+        content = get_input(filename)
+        total_score = get_total_score(content)
+
         print(f"\n{type_of_input}\n")
-        print(f"--> Part 1: ")
+        print(f"--> Part 1: Total Score according to Strategy Guide: {total_score}.")
         print(f"--> Part 2: ")
 
     print("\n======================\n")
