e83f310b142ee54b262f44b7738c6d0c6295153a
guemax <max@example.com>
2022-12-02 13:33:48 +0100

    Add main()

---
+++ day2/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+if __name__ == '__main__':
+    print("\n======================")
+
+    filenames = {"TEST INPUT": "day1/test_input.txt", "REAL INPUT": "day1/input.txt"}
+    for type_of_input, filename in filenames.items():
+        print(f"\n{type_of_input}\n")
+        print(f"--> Part 1: ")
+        print(f"--> Part 2: ")
+
+    print("\n======================\n")
