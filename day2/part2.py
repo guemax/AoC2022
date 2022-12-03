112889c28bae7e6fd34a6318a423595793d37492
guemax <max@example.com>
2022-12-03 11:22:06 +0100

    Refactor Day two Part 2

--- day2/part2.py
+++ day2/part2.py
@@ -5,45 +5,28 @@ def get_total_score(strategy_guide: str) -> int:
     total_score_of_game = 0
 
     for line in strategy_guide.splitlines():
-        print(line, end="")
-        line = line.split(" ")
-        their_choice, expected_outcome = from_letter_to_score_of_move(line[0]), from_letter_to_expected_outcome(line[1])
-        print(f" : {their_choice}, {expected_outcome}")
+        their_choice, expected_outcome = list(map(from_letter_to_value, line.split(" ")))
+        my_choice = 0
 
         if expected_outcome == 3:
-            total_score_of_game += their_choice
+            my_choice = their_choice
         elif expected_outcome == 6:
-            my_choice = 0
-            if their_choice == 1:  # Rock
-                my_choice = 2  # Paper
-            elif their_choice == 2:  # Paper
-                my_choice = 3  # Scissors
-            elif their_choice == 3:  # Scissors
-                my_choice = 1  # Rock
-            total_score_of_game += my_choice
+            my_choice = their_choice + 1 if their_choice != 3 else 1
         elif expected_outcome == 0:
-            my_choice = 0
-            if their_choice == 1:  # Rock
-                my_choice = 3  # Scissors
-            elif their_choice == 2:  # Paper
-                my_choice = 1  # Rock
-            elif their_choice == 3:  # Scissors
-                my_choice = 2  # Paper
-            total_score_of_game += my_choice
+            my_choice = their_choice - 1 if their_choice != 1 else 3
 
-        total_score_of_game += expected_outcome
+        total_score_of_game += my_choice + expected_outcome
 
     return total_score_of_game
 
 
-def from_letter_to_expected_outcome(letter: str) -> int:
-    if letter == "X":
-        return 0
-    elif letter == "Y":
-        return 3
-    elif letter == "Z":
-        return 6
+def from_letter_to_value(letter: str) -> int:
+    if is_their_move(letter):
+        return ord(letter) - ord("A") + 1
+    else:  # Is the expected outcome of this round
+        return (ord(letter) - ord("X")) * 3
 
 
-def from_letter_to_score_of_move(letter: str) -> int:
-    return ord(letter) - ord("A") + 1
+def is_their_move(letter: str) -> bool:
+    return ord(letter) <= 67  # ord("C")
+
