19636391545b1b0dda2050e4e46c422314cacfd4
guemax <max@example.com>
2022-12-03 10:04:59 +0100

    Working solution for day two
    Really rude if statements, but the result for the real input was now correct

--- day2/__main__.py
+++ day2/__main__.py
@@ -3,31 +3,33 @@
 import numpy as np
 
 
-def convert_letter_of_symbol_to_code(letter: str) -> np.int8:
-    captial_c = 67
-    offset_base = "A" if ord(letter) <= captial_c else "X"
-
-    return np.int8(ord(letter) + 1 - ord(offset_base))
+def score(letter: str) -> int:
+    if letter == "A" or letter == "X":
+        return 1
+    elif letter == "B" or letter == "Y":
+        return 2
+    elif letter == "C" or letter == "Z":
+        return 3
 
 
 def get_total_score(strategy_guide: str) -> int:
     total_score_of_game = 0
 
     for line in strategy_guide.splitlines():
-        their_choice, my_choice = list(map(convert_letter_of_symbol_to_code, line.split(" ")))
-        print(f"{line} -> {their_choice}, {my_choice}", end="")
+        their_choice, my_choice = line.split(" ")
 
-        if my_choice == their_choice:
-            current_score = my_choice + 3
-            print(f" === Score: {my_choice} + 3 == {current_score}")
-        elif my_choice > their_choice:
-            current_score = my_choice + 6
-            print(f" === Score: {my_choice} + 6 == {current_score}")
-        else:
-            current_score = my_choice
-            print(f" === Score: {my_choice} + 0 == {current_score}")
-
-        total_score_of_game += current_score
+        if (their_choice == "A" and my_choice == "X") \
+                or (their_choice == "B" and my_choice == "Y") \
+                or (their_choice == "C" and my_choice == "Z"):
+            total_score_of_game += score(my_choice) + 3
+        elif (their_choice == "A" and my_choice == "Y") \
+                or (their_choice == "B" and my_choice == "Z") \
+                or (their_choice == "C" and my_choice == "X"):
+            total_score_of_game += score(my_choice) + 6
+        elif (their_choice == "A" and my_choice == "Z") \
+                or (their_choice == "B" and my_choice == "X") \
+                or (their_choice == "C" and my_choice == "Y"):
+            total_score_of_game += score(my_choice)
 
     return total_score_of_game
 
@@ -48,5 +50,6 @@ if __name__ == '__main__':
         print(f"\n{type_of_input}\n")
         print(f"--> Part 1: Total Score according to Strategy Guide: {total_score}.")
         print(f"--> Part 2: ")
+
 
     print("\n======================\n")
