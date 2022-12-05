a4b3cbcf01a67caec470709b7cd752d97c25e8cc
guemax <max@example.com>
2022-12-05 17:54:24 +0100

    Add workking solution for day three part 2

--- day3/__main__.py
+++ day3/__main__.py
@@ -17,6 +17,31 @@ def get_sum_of_priorities(rucksacks: str) -> int:
     return total_priorities
 
 
+def get_sum_of_priorities_part2(rucksacks: str) -> int:
+    total_priorities = 0
+    rucksacks_of_group = []
+    i = 0
+
+    for rucksack in rucksacks.splitlines():
+        rucksacks_of_group.append(rucksack)
+        i += 1
+
+        if i == 3:
+            intersecting_letter = ''.join(
+                set(rucksacks_of_group[0]) & set(rucksacks_of_group[1]) & set(rucksacks_of_group[2])
+            )
+            print(intersecting_letter)
+            current_priority = ord(intersecting_letter.lower()) - ord("a") + 1
+            if intersecting_letter.isupper():
+                current_priority += 26
+
+            total_priorities += current_priority
+            rucksacks_of_group = []
+            i = 0
+
+    return total_priorities
+
+
 def get_input(file: str) -> str:
     with open(file, "r") as f:
         return f.read()
@@ -31,15 +56,20 @@ if __name__ == '__main__':
 
     for type_of_input, filename in filenames.items():
         content = get_input(filename)
-        sum_of_priorities = get_sum_of_priorities(content)
 
-        result_part1.append(sum_of_priorities)
+        sum_of_priorities_p1 = get_sum_of_priorities(content)
+        sum_of_priorities_p2 = get_sum_of_priorities_part2(content)
+
+        result_part1.append(sum_of_priorities_p1)
+        result_part2.append(sum_of_priorities_p2)
 
         print(f"\n{type_of_input}\n")
-        print(f"--> Part 1: The sum of the priorities the items is {sum_of_priorities}.")
-        print(f"--> Part 2: .")
+        print(f"--> Part 1: The sum of the priorities of all elves is {sum_of_priorities_p1}.")
+        print(f"--> Part 2: The sum of the priorities of all groups is {sum_of_priorities_p2}.")
 
     assert result_part1[0] == 157
     assert result_part1[1] == 8039
+
+    assert result_part2[0] == 70
 
     print("\n======================\n")
